64e54d541843e47f296681d4475c49bc16357f74
Saibo Geng <guncyber@yahoo.com>
2023-08-28 23:06:26 +0200

    test: add tests for gf_runtime

--- tests/test_gf_runtime.py
+++ tests/test_gf_runtime.py
@@ -36,3 +36,93 @@ class TestGFServerRuntime(TestCase):
                 "wine",
             ],
         )
+
+
+class TestGFServerRuntimeIE(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        pgf_dir = (
+            "/Users/saibo/Research/Projects/GCD/GF_editor/output/grammars/autogen/pgf"
+        )
+        task = "IE"
+        grammar_type = "fe"
+        dataset = "wikinre"
+        grammar_name = f"{task}_{grammar_type}_{dataset}_0.pgf"
+        relative_path = f"{task}/{grammar_type}/{dataset}/{grammar_name}"
+        self.gf_server = GFServerRuntime(default_pgf=relative_path, grammar_dir=pgf_dir)
+
+    def tearDown(self) -> None:
+
+        super().tearDown()
+        self.gf_server.clear()
+
+    def test_complete(self):
+        completions: List[str] = self.gf_server.complete(input_tokens=[])
+
+        self.assertEqual(completions, ["2", "518"])
+
+        completions: List[str] = self.gf_server.complete(input_tokens=["518"])
+        self.assertEqual(completions, ["29879"])
+
+        completions: List[str] = self.gf_server.complete(input_tokens=["518", "29879"])
+        self.assertEqual(completions, ["29962"])
+
+        completions: List[str] = self.gf_server.complete(
+            input_tokens=["518", "29879", "29962"]
+        )
+        self.assertEqual(len(completions), 3572)
+
+
+class TestGFServerRuntimeED(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        pgf_dir = (
+            "/Users/saibo/Research/Projects/GCD/GF_editor/output/grammars/autogen/pgf"
+        )
+        task = "ED"
+        grammar_type = "canonical"
+        dataset = "aida"
+        grammar_name = f"{task}_{grammar_type}_{dataset}_0.pgf"
+        relative_path = f"{task}/{grammar_type}/{dataset}/{grammar_name}"
+        self.gf_server = GFServerRuntime(default_pgf=relative_path, grammar_dir=pgf_dir)
+
+    def tearDown(self) -> None:
+
+        super().tearDown()
+        self.gf_server.clear()
+
+    def test_complete(self):
+        completions: List[str] = self.gf_server.complete(input_tokens=[])
+
+        self.assertEqual(completions, ["5546"])
+
+        completions: List[str] = self.gf_server.complete(input_tokens=["5546"])
+        self.assertEqual(completions, ["584"])
+
+        completions: List[str] = self.gf_server.complete(input_tokens=["5546", "584"])
+        self.assertEqual(completions, ["1815"])
+
+        completions: List[str] = self.gf_server.complete(
+            input_tokens=["5546", "584", "1815"]
+        )
+        self.assertEqual(completions, ["265"])
+
+        completions: List[str] = self.gf_server.complete(
+            input_tokens=["5546", "584", "1815", "265"]
+        )
+        self.assertEqual(completions, ["936"])
+
+        completions: List[str] = self.gf_server.complete(
+            input_tokens=["5546", "584", "1815", "265", "936"]
+        )
+        self.assertEqual(completions, ["3812"])
+
+        completions: List[str] = self.gf_server.complete(
+            input_tokens=["5546", "584", "1815", "265", "936", "3812"]
+        )
+        self.assertEqual(completions, ["518"])
+
+        completions: List[str] = self.gf_server.complete(
+            input_tokens=["5546", "584", "1815", "265", "936", "3812", "518"]
+        )
+        self.assertEqual(len(completions), 32)
